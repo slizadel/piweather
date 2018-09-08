af4b6c5edeaa580ab34327ab9e34832b0072d27a
Slade <slade.cozart@gmail.com>
2018-09-08 03:24:37 +0000

    First commit

---
+++ temp.py
@@ -0,0 +1,16 @@
+import bme280
+import smbus2
+from time import sleep
+
+
+port = 1
+addrees = 0x77 # adafruit bme280 address. 
+
+while true:
+	bme280_data = bme280.sample(bus,address)
+	humidity = bme280_data.humidity
+	pressure = bme280_data pressure
+	ambeint_temperature = bme280_data.temperature
+	print(humidity, pressure, ambeint_temperature)
+	sleep(1)
+
